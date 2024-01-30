0790d20806b59322d53e671faee67f9a77266858
Praise Abu <67503488+abupraise@users.noreply.github.com>
2024-01-30 15:59:28 +0100

    Update Testing.java

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -6,44 +6,51 @@ public class Testing {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Random random = new Random();
-        System.out.println("Enter your name: ");
+        System.out.print("Please Enter Your Name: ");
         String name = input.nextLine();
-        System.out.println("Welcome to the Mathiz App, " + name + "!");
+        System.out.println("Welcome to the Math-Eazi App, " + name + "!");
+        System.out.println("Answer the following Maths Questions");
+
         int score = 0;
         for (int i = 0; i < 5; i++) {
-            int a = random.nextInt(10);
-            int b = random.nextInt(10);
+            int number1 = random.nextInt(10);
+            int number2 = random.nextInt(10);
             int operation = random.nextInt(4);
             double correctAnswer = 0;
             String question = "Question " + (i + 1) + ": ";
             switch (operation) {
                 case 0:
-                    correctAnswer = a + b;
-                    question += a + " + " + b;
+                    correctAnswer = number1 + number2;
+                    question += number1 + " + " + number2 + " = ";
                     break;
                 case 1:
-                    correctAnswer = a - b;
-                    question += a + " - " + b;
+                    correctAnswer = number1 - number2;
+                    question += number1 + " - " + number2 + " = ";
                     break;
                 case 2:
-                    correctAnswer = a * b;
-                    question += a + " * " + b;
+                    correctAnswer = number1 * number2;
+                    question += number1 + " * " + number2 + " = ";
                     break;
                 case 3:
-                    if (b == 0) {
-                        b = 1;
+                    if (number2 == 0) {
+                        number2 = 1;
                     }
-                    correctAnswer = (double) a / b;
-                    question += a + " / " + b;
+                    correctAnswer = (double) number1 / number2;
+                    question += number1 + " / " + number2 + " = ";
                     break;
             }
-            System.out.println(question);
+            System.out.print(question);
             double userAnswer = input.nextDouble();
+
             if (userAnswer == correctAnswer) {
+                System.out.println("Amazing! You are right.");
                 score++;
+            } else {
+                System.out.println("Ohhh Nooo. The correct answer is: " + correctAnswer);
             }
         }
-        System.out.println(name + ", your final score is: " + score + "/5");
+
+        System.out.println(name + ", Your Total Score Is: " + score + "/5");
         input.close();
     }
 }
